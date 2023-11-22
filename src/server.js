df82a95b34b7e2cd45d1f77e8df525e9fcffacc6
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-22 14:49:17 +0700

    init project

---
+++ src/server.js
@@ -0,0 +1,14 @@
+// const express = require('express')
+import express from "express";
+const app = express();
+
+const hostname = "localhost";
+const port = 8017;
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
