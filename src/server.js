f73a7067745d75a72dd42f61328b6acb40bfa3ba
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-22 15:51:25 +0700

    Cấu trúc dự án nâng cao, chuẩn thực tế

--- src/server.js
+++ src/server.js
@@ -1,14 +1,30 @@
-// const express = require('express')
 import express from "express";
+import { mapOrder } from "~/utils/sorts.js";
+
 const app = express();
 
 const hostname = "localhost";
 const port = 8017;
 
-app.get("/", function (req, res) {
-    res.send("Hello World!");
+app.get("/", (req, res) => {
+  // Test Absolute import mapOrder
+  console.log(
+    mapOrder(
+      [
+        { id: "id-1", name: "One" },
+        { id: "id-2", name: "Two" },
+        { id: "id-3", name: "Three" },
+        { id: "id-4", name: "Four" },
+        { id: "id-5", name: "Five" },
+      ],
+      ["id-5", "id-4", "id-2", "id-3", "id-1"],
+      "id"
+    )
+  );
+  res.end("<h1>Hello World!</h1><hr>");
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  // eslint-disable-next-line no-console
+  console.log(`Hello Chi Hieu3 Dev, I am running at ${hostname}:${port}/`);
 });
