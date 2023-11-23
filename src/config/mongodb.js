725b1268c43814edd8652acd50bece8338510095
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-23 20:26:14 +0700

    Tối ưu kết nối MongoDB Atlas vào dự án

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,35 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+const MONGODB_URI =
+  "mongodb+srv://vanchihieu3:KPPnLY9EGEbIcYgk@cluster0-chihieudev.unhzc4t.mongodb.net/?retryWrites=true&w=majority";
+
+const DATABASE_NAME = "trello-chihieudev";
+
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+let trelloDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const CONNECT_DB = async () => {
+  try {
+    // gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+    await mongoClientInstance.connect();
+
+    // Kết nối thành công thì lấy ra Database theo tên và gán ngược nó lại cho biến trelloDatabaseInstance
+    trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME);
+    console.log("Connected successfully to server");
+  } catch (error) {
+    throw new Error("Connect failed", error);
+  }
+};
+
+// Luu y: Ham nay phai duoc goi sau khi da goi ham CONNECT_DB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance)
+    throw new Error("Must connect to Database first!");
+};
--- src/server.js
+++ src/server.js
@@ -1,30 +1,39 @@
+/* eslint-disable no-console */
 import express from "express";
-import { mapOrder } from "~/utils/sorts.js";
+import { CONNECT_DB, GET_DB } from "./config/mongodb";
 
-const app = express();
+const START_SERVER = () => {
+  const app = express();
 
-const hostname = "localhost";
-const port = 8017;
+  const hostname = "localhost";
+  const port = 8017;
 
-app.get("/", (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(
-    mapOrder(
-      [
-        { id: "id-1", name: "One" },
-        { id: "id-2", name: "Two" },
-        { id: "id-3", name: "Three" },
-        { id: "id-4", name: "Four" },
-        { id: "id-5", name: "Five" },
-      ],
-      ["id-5", "id-4", "id-2", "id-3", "id-1"],
-      "id"
-    )
-  );
-  res.end("<h1>Hello World!</h1><hr>");
-});
+  app.get("/", (req, res) => {
+    res.end("<h1>Hello World!</h1><hr>");
+  });
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Chi Hieu3 Dev, I am running at ${hostname}:${port}/`);
-});
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello Chi Hieu3 Dev, I am running at ${hostname}:${port}/`);
+  });
+};
+
+//  IIFE (Immediately Invoked Function Expression)
+(async () => {
+  try {
+    await CONNECT_DB;
+
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
+
+// CONNECT_DB();
+// .then(() => console.log("Server started"))
+// .then(() => START_SERVER())
+// .catch((error) => {
+//   console.error(error);
+//   process.exit(0);
+// });
