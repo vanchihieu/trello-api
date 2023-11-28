4f0ee854e8e7198bc87f25710eb3a95ed99cda2c
Van Chi Hieu <vanchihieu3@gmail.com>
2023-11-28 13:43:32 +0700

    check data ở cả Validation và Model Schema

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -1,4 +1,5 @@
 import Joi from "joi";
+import { ObjectId } from "mongodb";
 import { GET_DB } from "~/config/mongodb";
 import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from "~/utils/validators";
 
@@ -18,11 +19,21 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false),
 });
 
+const validateBeforeCreate = async (data) => {
+  // chỉ định abortEarly: false để bắt tất cả các lỗiv
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, {
+    abortEarly: false,
+  });
+};
+
 const createNew = async (data) => {
   try {
+    const validData = await validateBeforeCreate(data);
+    console.log("🚀 ~ createNew ~ validData:", validData);
+
     const createdBoard = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .insertOne(data);
+      .insertOne(validData);
 
     return createdBoard;
   } catch (error) {
@@ -34,7 +45,7 @@ const findOneById = async (id) => {
   try {
     const result = await GET_DB()
       .collection(BOARD_COLLECTION_NAME)
-      .findOne({ _id: id });
+      .findOne({ _id: new ObjectId(id) });
 
     return result;
   } catch (error) {
